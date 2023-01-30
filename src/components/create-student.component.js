ecbafb693af96d7994b9a82c57cbf6da44964653
miked <mdidia@gravitylending.com>
2023-01-29 19:33:45 -0600

    initial commit

---
+++ src/components/create-student.component.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+export default class CreateStudent extends Component {
+	constructor(props) {
+		super(props);
+		// Setting up functions
+		this.onChangeStudentName = this.onChangeStudentName.bind(this);
+		this.onChangeStudentEmail = this.onChangeStudentEmail.bind(this);
+		this.onChangeStudentRollno = this.onChangeStudentRollno.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+		// Setting up state
+		this.state = {
+			name: "",
+			email: "",
+			rollno: "",
+		};
+	}
+	onChangeStudentName(e) {
+		this.setState({ name: e.target.value });
+	}
+	onChangeStudentEmail(e) {
+		this.setState({ email: e.target.value });
+	}
+	onChangeStudentRollno(e) {
+		this.setState({ rollno: e.target.value });
+	}
+	onSubmit(e) {
+		e.preventDefault();
+		const studentObject = {
+			name: this.state.name,
+			email: this.state.email,
+			rollno: this.state.rollno,
+		};
+		axios
+			.post(
+				"http://localhost:4000/students/create-student",
+				studentObject
+			)
+			.then((res) => console.log(res.data));
+		this.setState({ name: "", email: "", rollno: "" });
+	}
+	render() {
+		return (
+			<div className="form-wrapper">
+				<Form onSubmit={this.onSubmit}>
+					<Form.Group controlId="Name">
+						<Form.Label>Snippet Name</Form.Label>
+						<Form.Control
+							type="text"
+							value={this.state.name}
+							onChange={this.onChangeStudentName}
+						/>
+					</Form.Group>
+					<Form.Group controlId="Email">
+						<Form.Label>Keywords</Form.Label>
+						<Form.Control
+							type="text"
+							value={this.state.email}
+							onChange={this.onChangeStudentEmail}
+						/>
+					</Form.Group>
+					<Form.Group controlId="Name">
+						<Form.Label>Code</Form.Label>
+						<Form.Control
+							type="text"
+							as="textarea"
+							rows={3}
+							value={this.state.rollno}
+							onChange={this.onChangeStudentRollno}
+						/>
+					</Form.Group>
+					<Button
+						variant="danger"
+						size="lg"
+						block="block"
+						type="submit"
+						className="mt-4"
+					>
+						Create Snippet
+					</Button>
+				</Form>
+			</div>
+		);
+	}
+}
